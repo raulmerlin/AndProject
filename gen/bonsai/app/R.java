905d44bd5ec1fbaea9ad50a30f6679b08b8b96c6
Merlin <merlin@w217059.ccupm.upm.es>
2012-05-14 11:32:21 +0200

    Suprimido margen del main, creada pagina de edicion de bonsais con boton de seleccion de imagen y casi implementado (falta)

--- gen/bonsai/app/R.java
+++ gen/bonsai/app/R.java
@@ -8,6 +8,9 @@
 package bonsai.app;
 
 public final class R {
+    public static final class array {
+        public static final int situation_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -17,25 +20,25 @@ public final class R {
         public static final int ic_tab_more=0x7f020003;
     }
     public static final class id {
-        public static final int analogClock1=0x7f050003;
-        public static final int cancelButton=0x7f050009;
-        public static final int donatebutton=0x7f050010;
-        public static final int editAge=0x7f05000c;
-        public static final int editFamily=0x7f05000b;
-        public static final int editHeight=0x7f05000d;
-        public static final int editName=0x7f05000a;
-        public static final int editPhoto=0x7f05000e;
-        public static final int editSituation=0x7f05000f;
-        public static final int footer=0x7f050005;
-        public static final int imageButton1=0x7f050000;
-        public static final int imageButton2=0x7f050007;
-        public static final int imageButton3=0x7f050006;
-        public static final int imageView1=0x7f050011;
-        public static final int saveButton=0x7f050008;
-        public static final int startbutton=0x7f050012;
-        public static final int textView1=0x7f050001;
-        public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050004;
+        public static final int analogClock1=0x7f060003;
+        public static final int button1=0x7f06000e;
+        public static final int cancelButton=0x7f060009;
+        public static final int donatebutton=0x7f060010;
+        public static final int editAge=0x7f06000c;
+        public static final int editFamily=0x7f06000b;
+        public static final int editHeight=0x7f06000d;
+        public static final int editName=0x7f06000a;
+        public static final int footer=0x7f060005;
+        public static final int imageButton1=0x7f060000;
+        public static final int imageButton2=0x7f060007;
+        public static final int imageButton3=0x7f060006;
+        public static final int imageView1=0x7f060011;
+        public static final int saveButton=0x7f060008;
+        public static final int spinner1=0x7f06000f;
+        public static final int startbutton=0x7f060012;
+        public static final int textView1=0x7f060001;
+        public static final int textView2=0x7f060002;
+        public static final int textView3=0x7f060004;
     }
     public static final class layout {
         public static final int bonsai=0x7f030000;
--- src/bonsai/app/EditBonsaiActivity.java
+++ src/bonsai/app/EditBonsaiActivity.java
@@ -1,16 +1,37 @@
 package bonsai.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class EditBonsaiActivity extends Activity {
 	
-
     // Utilidad de manejo de Base de Datos
 	private BonsaiDbUtil bonsaidb;
+
+	private EditText editName;
+	private EditText editFamily;
+	private EditText editAge;
+	private EditText editHeight;
+	private Spinner editSituation;
+	
+	private String name;
+	private int family_id;
+	private int age;
+	private int height;
+	private String photo;
+	private long last_pode;
+	private long last_water;
+	private long last_transplant;
+	private String situation;
 	
 	
 	
@@ -20,8 +41,24 @@ public class EditBonsaiActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editbonsai);
+        
+
+        editName= (EditText)findViewById(R.id.editName);
+        editFamily= (EditText)findViewById(R.id.editFamily);
+        editAge= (EditText)findViewById(R.id.editAge);
+        editHeight= (EditText)findViewById(R.id.editHeight);
+        
+        editSituation = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.situation_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        editSituation.setAdapter(adapter);
+        
+        
+        
         bonsaidb = new BonsaiDbUtil(this);	// Construinos el DDBBAdapter
         bonsaidb.open();
+
         try {
         	Cursor bonsai = bonsaidb.fetchBonsai(0);
             startManagingCursor(bonsai);
@@ -36,9 +73,50 @@ public class EditBonsaiActivity extends Activity {
         
     }
     
+    public void selectImage(View v) {
+    	   // TODO Auto-generated method stub
+    	   Intent intent = new Intent(Intent.ACTION_PICK,
+    	     android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+    	   startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+     // TODO Auto-generated method stub
+     super.onActivityResult(requestCode, resultCode, data);
+
+     if (resultCode == RESULT_OK){
+      Uri targetUri = data.getData();
+  	  Toast.makeText(this, targetUri.toString(), Toast.LENGTH_LONG).show();
+  	  photo = targetUri.toString();
+     }
+    }
     
+    
+    public void goSave(View v) {
+    	try {
+    	name =  editName.getText().toString();
+    	family_id =  Integer.parseInt(editFamily.getText().toString());
+    	age =  Integer.parseInt(editAge.getText().toString());
+    	height =  Integer.parseInt(editHeight.getText().toString());
+    	situation = editSituation.getSelectedItem().toString();
+    	} catch(Exception e) {
+    		Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+    	}
+    	if((name.length() > 1) && (situation != null)) {
+    		Toast.makeText(this, name + " changed.", Toast.LENGTH_LONG).show();
+    		finish();
+    	} else {
+    	}
+    }
+    
+    public void goDelete(View v) {
+    	Toast.makeText(this, "DELETE - Still not Implemented", Toast.LENGTH_LONG).show();
+    	finish();
+    }
     
     public void goCancel(View v) {
+    	Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show();
     	finish();
     }
 }
