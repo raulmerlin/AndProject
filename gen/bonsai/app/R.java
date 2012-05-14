a93dfa75f12ea36e771bf44cabe0075cbe9d6f0d
Merlin <merlin@w217059.ccupm.upm.es>
2012-05-14 15:18:52 +0200

    Creada lista de Bonsais

--- gen/bonsai/app/R.java
+++ gen/bonsai/app/R.java
@@ -22,34 +22,35 @@ public final class R {
     }
     public static final class id {
         public static final int analogClock1=0x7f060003;
-        public static final int button1=0x7f06000f;
-        public static final int cancelButton=0x7f060009;
-        public static final int donatebutton=0x7f060011;
-        public static final int editAge=0x7f06000c;
-        public static final int editFamily=0x7f06000b;
-        public static final int editHeight=0x7f06000d;
-        public static final int editName=0x7f06000a;
+        public static final int bonsairowtext=0x7f060008;
+        public static final int button1=0x7f060010;
+        public static final int cancelButton=0x7f06000a;
+        public static final int donatebutton=0x7f060012;
+        public static final int editAge=0x7f06000d;
+        public static final int editFamily=0x7f06000c;
+        public static final int editHeight=0x7f06000e;
+        public static final int editName=0x7f06000b;
         public static final int footer=0x7f060005;
         public static final int imageButton1=0x7f060000;
         public static final int imageButton2=0x7f060007;
         public static final int imageButton3=0x7f060006;
         public static final int imageView1=0x7f060013;
-        public static final int photoURLtext=0x7f06000e;
-        public static final int saveButton=0x7f060008;
-        public static final int scrollView1=0x7f060012;
-        public static final int spinner1=0x7f060010;
+        public static final int photoURLtext=0x7f06000f;
+        public static final int saveButton=0x7f060009;
+        public static final int spinner1=0x7f060011;
         public static final int textView1=0x7f060001;
         public static final int textView2=0x7f060002;
         public static final int textView3=0x7f060004;
     }
     public static final class layout {
         public static final int bonsai=0x7f030000;
-        public static final int calendar=0x7f030001;
-        public static final int editbonsai=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int more=0x7f030004;
-        public static final int selectbonsai=0x7f030005;
-        public static final int start=0x7f030006;
+        public static final int bonsai_row=0x7f030001;
+        public static final int calendar=0x7f030002;
+        public static final int editbonsai=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int more=0x7f030005;
+        public static final int selectbonsai=0x7f030006;
+        public static final int start=0x7f030007;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- src/bonsai/app/SelectBonsaiActivity.java
+++ src/bonsai/app/SelectBonsaiActivity.java
@@ -1,22 +1,42 @@
 package bonsai.app;
 
+
+
+
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-public class SelectBonsaiActivity extends Activity {
+public class SelectBonsaiActivity extends ListActivity {
 	
-	
+
+    // Utilidad de manejo de Base de Datos
+	private BonsaiDbUtil bonsaidb;
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.selectbonsai);
+        bonsaidb = new BonsaiDbUtil(this);	// Construinos el DDBBAdapter
+        bonsaidb.open();						// 
+
+
+        Cursor bonsaisCursor = bonsaidb.fetchAllBonsais();
+        startManagingCursor(bonsaisCursor);
+        String[] from = new String[]{BonsaiDbUtil.KEY_NAME};
+        int[] to = new int[]{R.id.bonsairowtext};
+        // Now create a simple cursor adapter and set it to display
+        SimpleCursorAdapter bonsais = 
+            new SimpleCursorAdapter(this, R.layout.bonsai_row, bonsaisCursor, from, to);
+        setListAdapter(bonsais);
+        
+        
         
         
     }
