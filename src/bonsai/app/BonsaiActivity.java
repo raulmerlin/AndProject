cf6460e08cecc50e358f4f35ba903647bc13aa05
Ruben Valero <rubevalero@gmail.com>
2012-05-16 17:45:02 +0200

    agregada temperatura a la actividad bonsaiActivity

--- src/bonsai/app/BonsaiActivity.java
+++ src/bonsai/app/BonsaiActivity.java
@@ -1,6 +1,9 @@
 package bonsai.app;
 
 
+import java.util.List;
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -11,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 import android.widget.Toast;
+import bonsai.app.weather.*;
 
 public class BonsaiActivity extends Activity {
 	
@@ -22,6 +26,12 @@ public class BonsaiActivity extends Activity {
 	private TextView family;
 	private ImageView photo;
 	
+	//Será un campo de la base de datos que se rellena al crear un bonsai,
+	//con el formato códigoPostal,Codígo Pais o códigoPostal,Pais
+	//de momento para el ejemplo pruebo con:
+	private String location = "28040,ES";
+	private TextView textWeather;
+
 	
     /** Called when the activity is first created. */
     @Override
@@ -33,6 +43,7 @@ public class BonsaiActivity extends Activity {
         name = (TextView)findViewById(R.id.textName);
         family = (TextView)findViewById(R.id.textFamily);
         photo = (ImageView)findViewById(R.id.bonsaiImage);
+        textWeather =(TextView)findViewById(R.id.textweather);
         
         bonsaidb = new BonsaiDbUtil(this);	// Construinos el DDBBAdapter
         bonsaidb.open();
@@ -49,6 +60,14 @@ public class BonsaiActivity extends Activity {
             if(photouri.length() > 1)
             	photo.setImageURI(Uri.parse(photouri));
             else photo.setImageResource(R.drawable.ic_launcher);
+            
+            
+            //añado las sentencias que determinarán el tiempo atmosférico
+            XmlParserSax saxparser =
+                    new XmlParserSax("http://www.google.com/ig/api?weather="+location);
+            List<Weather> weather = saxparser.parse();
+            Weather w=weather.get(0);
+            textWeather.setText(Double.toString(w.getTempMedia())+"º");
         	
             
         } catch (Exception e) {
