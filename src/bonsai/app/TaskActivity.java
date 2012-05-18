630c8174ec35fe855ec130600aa64aa2e9109336
Merlin <merlin@MacBook-Air-de-Merlin.local>
2012-05-18 08:23:08 +0200

    Depurada la logica de la lista de tareas, funcionando.

--- src/bonsai/app/TaskActivity.java
+++ src/bonsai/app/TaskActivity.java
@@ -22,39 +22,7 @@ public class TaskActivity extends ListActivity {
         setContentView(R.layout.task);
         
         
-        bonsaidb = new BonsaiDbUtil(this);	// Construinos el DDBBAdapter
-        bonsaidb.open();
-        familydb = new FamilyDbUtil(this);	// Construinos el DDBBAdapter
-        familydb.open();
-        
-        Cursor bonsaisCursor = bonsaidb.fetchAllBonsais();
-        startManagingCursor(bonsaisCursor);
-
-        tasks = new String[bonsaisCursor.getCount() * 3];
-        cont = 0;
-        
-        bonsaisCursor.moveToFirst();
-        for(int i = 0; i < bonsaisCursor.getCount(); i++) {
-        	long id = bonsaisCursor.getLong(bonsaisCursor.getColumnIndexOrThrow(BonsaiDbUtil.KEY_ROWID));
-        	String possibletask = checkWater(id);
-        	if(possibletask != null) {
-        		tasks[cont] = possibletask;
-        		cont++;
-        	}
-        	possibletask = checkPode(id);
-        	if(possibletask != null) {
-        		tasks[cont] = possibletask;
-        		cont++;
-        	}
-        	possibletask = checkTransplant(id);
-        	if(possibletask != null) {
-        		tasks[cont] = possibletask;
-        		cont++;
-        	}
-        	if(i < bonsaisCursor.getCount()-1) bonsaisCursor.moveToNext();
-        }
-
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.task_row, tasks));
+     
 
      //   ListView lv = getListView();
      //   lv.setTextFilterEnabled(true);
@@ -62,9 +30,61 @@ public class TaskActivity extends ListActivity {
         
         
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+    	   bonsaidb = new BonsaiDbUtil(this);	// Construinos el DDBBAdapter
+           bonsaidb.open();
+           familydb = new FamilyDbUtil(this);	// Construinos el DDBBAdapter
+           familydb.open();
+           
+           Cursor bonsaisCursor = bonsaidb.fetchAllBonsais();
+           startManagingCursor(bonsaisCursor);
+
+           tasks = new String[bonsaisCursor.getCount() * 3];
+           cont = 0;
+           
+           bonsaisCursor.moveToFirst();
+           for(int i = 0; i < bonsaisCursor.getCount(); i++) {
+           	long id = bonsaisCursor.getLong(bonsaisCursor.getColumnIndexOrThrow(BonsaiDbUtil.KEY_ROWID));
+           	String possibletask = checkWater(id);
+           	if(possibletask != null) {
+           		tasks[cont] = possibletask;
+           		cont++;
+           	}
+           	possibletask = checkPode(id);
+           	if(possibletask != null) {
+           		tasks[cont] = possibletask;
+           		cont++;
+           	}
+           	possibletask = checkTransplant(id);
+           	if(possibletask != null) {
+           		tasks[cont] = possibletask;
+           		cont++;
+           	}
+           	if(i < bonsaisCursor.getCount()-1) bonsaisCursor.moveToNext();
+           }
+           
+           int cuenta = 0;
+           for(int i = 0; i < tasks.length; i++) {
+        	   if(tasks[i] != null) cuenta++;
+           }
+           int situac = 0;
+           String[] finaltasks = new String[cuenta];
+           for(int i = 0; i < tasks.length; i++){
+        	   if(tasks[i] != null) {
+        		   finaltasks[situac] = tasks[i];
+        		   situac++;
+        	   }
+           }
+           
+           	setListAdapter(new ArrayAdapter<String>(this, R.layout.task_row, finaltasks));
+           
+    }
     
 
     private String checkWater(long id) {
+    	String name;
     	String family;
     	long lastwatered;
     	long waterfrec;
@@ -76,6 +96,7 @@ public class TaskActivity extends ListActivity {
         try {
         	Cursor bonsai = bonsaidb.fetchBonsai(id);
         	startManagingCursor(bonsai);
+        	name = bonsai.getString(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_NAME));
         	family = bonsai.getString(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_FAMILY));
         	lastwatered = bonsai.getInt(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_LAST_WATER));
         	height = bonsai.getInt(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_HEIGHT));
@@ -86,11 +107,11 @@ public class TaskActivity extends ListActivity {
         	
         	// LOGICA DE REGADO
         	
-        	if(lastwatered == 0) return("Never watered (or no info)");
-        	else if(temperature > 35) return("You should water 2 times with " + height/4 + " cl today.");
-        	else if(temperature < 0) return("It's really cold, you should not water today.");
-        	else if((hoursTime - lastwatered) > waterfrec) return("You should water today once with " + height/2 +" cl.");
-        	else return("Water OK. Last watered: " + new Date((long)(lastwatered * (1000*60*60))).toLocaleString().toString().substring(0,16));
+        	if(lastwatered == 0) return ("Set water info on " + name);
+        	else if(temperature > 35) return("Water " + name + " 2 times with " + height/4 + " cl.");
+        	else if(temperature < 0) return null;
+        	else if((hoursTime - lastwatered) > waterfrec) return("Water " + name + " today once with " + height/2 +" cl.");
+        	else return null;
         	
         } catch(Exception e) {
         	System.out.println(e.toString());
@@ -99,6 +120,7 @@ public class TaskActivity extends ListActivity {
     	
     }
     private String checkTransplant(long id) {
+    	String name;
     	String family;
     	long lasttransplant;
     	long transplantfrec;
@@ -110,6 +132,7 @@ public class TaskActivity extends ListActivity {
         try {
         	Cursor bonsai = bonsaidb.fetchBonsai(id);
         	startManagingCursor(bonsai);
+        	name = bonsai.getString(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_NAME));
         	family = bonsai.getString(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_FAMILY));
         	lasttransplant = bonsai.getInt(bonsai.getColumnIndexOrThrow(BonsaiDbUtil.KEY_LAST_TRASPLANT));
         	long date = new Date().getTime() / (1000*60*60);
@@ -123,12 +146,12 @@ public class TaskActivity extends ListActivity {
         	
         	// LOGICA DE TRANSPLANTE
         	
-        	if(age < 2) return ("You have a baby bonsai! Don't transplant it.");
-        	else if(lasttransplant == 0) return("Never transplanted (or no info).");
+        	if(age < 2) return null;
+        	else if(lasttransplant == 0) return("Set transplant info on " + name);
         	else {
-        		if((hoursTime - lasttransplant) > transplantfrec) return("You should transplant your bonsai." +
-        				"\nLast trasplant was " + (hoursTime - lasttransplant)/24 + " days ago.");
-        		else return("Transplant OK! Last transplant was " + (hoursTime - lasttransplant)/24 + " days ago.");
+        		if((hoursTime - lasttransplant) > transplantfrec) return("Transplant " + name + 
+        				"\nLast trasplant was " + (hoursTime - lasttransplant)/24 + " days ago");
+        		else return null;
         	}
         	
         } catch(Exception e) {
@@ -163,13 +186,13 @@ public class TaskActivity extends ListActivity {
         	podefrecuency = cfamily.getInt(cfamily.getColumnIndexOrThrow(FamilyDbUtil.KEY_PODE_FRECUENCY));
         	
         	// LOGICA DE TRANSPLANTE
-        	if(lastpode == 0) return("No info about " + name + " prunes.\nMaybe never pruned.");
+        	if(lastpode == 0) return("Set info about " + name + " prunes.");
         	else if(age < 2) {	// Los bonsais con menos de dos a–os se suelen defoliar al 50% cada 2 meses, aprox
-        		if(hoursTime - lastpode > 60 * 24) return("Defoliate your bonsai 50%");
-        		else return("Your bonsai prune is not necessary");
+        		if(hoursTime - lastpode > 60 * 24) return("Defoliate " + name + " 50%");
+        		else return null;
         	}
-        	else if(age >= 2 && (hoursTime - lastpode > podefrecuency)) return("You should defoliate " + name + " 20%");
-        	else return(name + " prune is not necessary");
+        	else if(age >= 2 && (hoursTime - lastpode > podefrecuency)) return("Defoliate " + name + " 20%");
+        	else return null;
         	
         } catch(Exception e) {
         	System.out.println(e.toString());
